279e95a7c11f5462b95d04528c8aab89aaf34812
Nicolàs Fredes <niko.afv@gmail.com>
2012-05-26 01:31:55 -0400

    Primer commit de la rama DVELOPMENT
    Se reemplazó el contenido del archivo website/index.php
    por solo una estructura basica de HTML5 para comenzar
    a partir desde ese punto a contruir el sitio.

--- website/index.php
+++ website/index.php
@@ -1,31 +1,21 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Larvaterapia</title>
-
-		<style>
-			body{
-				margin: 0;
-				background-color: #004c6c;
-			}
-			div{
-				margin: 100px 0 0 300px;
-				border: 1px solid #000000;
-				border-radius: 50px;
-				padding: 20px;
-				width: 600px;
-				height: 150px;
-				background: #006d9c;
-			}
-			div h1{
-				font-family: Helvetica;
-			}
-		</style>
-
 	</head>
 	<body>
-
-		<div>
-			<h1>En este sitio se publicar&aacute;n los avances estables del proyecto Larvaterapia</h1>
-		</div>
+		<section>
+			<header>
+			
+			</header>
+			
+			<section>
+			
+			</section>
+			
+			<footer>
+			
+			</footer>
+		</section>
 	</body>
 </html>
