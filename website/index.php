0b943da8c9deec563ff216e649ad9e7acd26c609
Nicolàs Fredes <niko.afv@gmail.com>
2012-05-27 03:15:31 -0400

    Commit's Info:
    - Se agregarón los directorios donde se incluirán las Hojas de Estilo (css)
      y donde se alojarán las imagenes (img).
    - Se agregarón ficheros del framework css 'BluePrint'.
    - Se agregarón las imagenes principales a utilizar en el sitio.
    - Se agregó un archivo de estilos 'estilos.css' que corresponde a la
      hoja de estilos principal del sitio.
    - Se modificó el index.php para empezar a dar forma al sitio.
    
    Nks <---
    27-05-2012

--- website/index.php
+++ website/index.php
@@ -1,21 +1,51 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Larvaterapia</title>
+<html lang="es">
+    <head>
+		
+        <title>Larvaterapia</title>
+        
+        <!--	Begin StyleSheet-->
+        <link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen" charset="utf-8"/>
+        <link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen" charset="utf-8"/>
+        <link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="screen" charset="utf-8"/>
+        <link rel="stylesheet" href="css/estilos.css" type="text/css" media="screen" charset="utf-8"/>
+        <!--	End StyleSheet-->
+		
 	</head>
-	<body>
-		<section>
-			<header>
-			
-			</header>
-			
-			<section>
-			
-			</section>
-			
-			<footer>
-			
-			</footer>
-		</section>
-	</body>
+    <body>
+        <section id="main">
+            <header>
+                <div class="container">
+                    <h1>Header</h1>
+                </div><!--  Div contanier-->
+                
+            </header><!--   Footer  -->
+            
+            <section id="main-content">
+                    <section id="banner">
+                        <div class="container">
+                            <h1>Banner</h1>
+                        </div>	
+                    </section><!--  Section banner  -->
+            
+                    <section id="content">
+                        <div class="separator">&nbsp;</div>
+                        <div class="container">
+                            <h1>Contenido</h1>
+                            <h1>Contenido</h1>
+                            <h1>Contenido</h1><br />
+                            <h1>Contenido</h1>
+                        </div>
+                        <div class="separator">&nbsp;</div>
+                    </section><!--  Section content -->
+                    
+            </section><!--  Section main-content    -->
+            
+            <footer>
+                <div class="container">
+                    <h1>Footer</h1>
+                </div>
+            </footer><!--   Footer  -->
+        </section><!--  Section main-content    -->
+    </body><!-- Body    -->
 </html>
