e661c6ac2f9279f1fb1e0e4b8a98cdfb98921f87
Nicolàs Fredes <niko.afv@gmail.com>
2012-05-29 01:11:23 -0400

    Commit's Info:
    Se Han eliminado del versionado los archivos:
    
    	deleted:    website/content.php
    	deleted:    website/footer.php
    	deleted:    website/header.php

--- website/footer.php
+++
@@ -1,9 +0,0 @@
-            <footer>
-                <div class="separator">&nbsp;</div>
-                <div class="container">
-                    <img id="footer-cont" class="prepend-1" src="img/png/home/logo adich mas texto.png"   />
-                </div>
-            </footer><!--   Footer  -->
-        </section><!--  Section main-content    -->
-    </body><!-- Body    -->
-</html>
