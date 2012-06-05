25a2573ddcb23dcbc0d84617233d5dbda98e5140
Nicolás Fredes <niko.afv@gmail.com>
2012-06-05 18:21:58 -0400

    Commit's Info
    - Se agregaron 2 imagenes para el banner
    - Se llamaron a las 2 imagenes para el banner

--- website/base/banner.php
+++ website/base/banner.php
@@ -2,13 +2,10 @@
     <div class="container">
         <div class="prepend-1 span-20 append-1" id="slider">
             <div>
-                <img src="./img/png/banner/banner.png"    />
+                <img src="./img/png/banner/banner01.png"    />
             </div>
             <div>
-                <img src="./img/png/banner/banner.png"    />
-            </div>
-            <div>
-                <img src="./img/png/banner/banner.png"    />
+                <img src="./img/png/banner/banner02.png"    />
             </div>
         </div>
     </div>  
