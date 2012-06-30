fa58742cac4c82ad9acd22a393b7ba3fdc623d12
Nicolás Fredes <niko.afv@gmail.com>
2012-06-30 01:21:35 -0400

    Commit's Info:
    - Se cambiaron las imagenes del banner para que fuera mas pequeño.

--- website/base/banner.php
+++ website/base/banner.php
@@ -1,14 +1,14 @@
 <div id="banner">
-    <div class="container">
+    <div class="container" id="contenedor">
         <div class="prepend-1 span-20 append-1" id="slider">
             <div>
-                <img src="./img/png/banner/banner00.png"    />
+                <img src="./img/png/banner/banner_00b.png"    />
             </div>
             <div>
-                <img src="./img/png/banner/banner01.png"    />
+                <img src="./img/png/banner/banner_01b.png"    />
             </div>
             <div>
-                <img src="./img/png/banner/banner02.png"    />
+                <img src="./img/png/banner/banner_02b.png"    />
             </div>
         </div>
     </div>  
